17f15fc9895f3aee2cd592997e36663346478b4f
FakeBird-AI <fakebird279@gmail.com>
2025-04-21 14:18:47 +0900

    Update archive.js
    ZIPリンク(1ヶ月より前のアーカイブです。Premiumにアップグレードすると閲覧可能です。)

--- archive.js
+++ archive.js
@@ -41,11 +41,11 @@ function getZipLinkContent(item) {
     case "1350114736242557010": // Special
       return recent
         ? `<a href="${item.url}" target="_blank">▶ ZIPリンク</a>`
-        : "1ヶ月より前のアーカイブです。Premiumにアップグレードすると閲覧可能です。";
+        : "ZIPリンク(1ヶ月より前のアーカイブです。Premiumにアップグレードすると閲覧可能です。)";
     case "1350114379391045692": // Standard
       return recent
         ? "PremiumもしくはSpecialにアップグレードすると閲覧可能です。"
-        : "1ヶ月より前のアーカイブです。Premiumにアップグレードすると閲覧可能です。";
+        : "ZIPリンク(1ヶ月より前のアーカイブです。Premiumにアップグレードすると閲覧可能です。)";
     default:
       return "このコンテンツにアクセスできません。";
   }
